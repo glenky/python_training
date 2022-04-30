9114aff1615781e3caefa54ec37995e33827bf2a
Maria <Maria@yandex.ru>
2022-04-30 14:23:28 +0300

    tests added: test_del_contact.py test_modify_contact.py test_modify_group.py
    fixture.contact, fixture.group are updated: methods modify_first_contact, delete_first_contact, modify_first_group added

--- fixrure/contact.py
+++ fixrure/contact.py
@@ -38,6 +38,55 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
+    def modify_first_contact(self, contact):
+        wd = self.app.wd
+        self.return_to_home_page()
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # press edit contact
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        # update contact details
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.home_page)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address2)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.work_phone)
+        # submit update
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        self.return_to_home_page()
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to.alert.accept()
+        self.return_to_home_page()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
---
+++ test/test_modify_contact.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_modify_contact(app):
+    app.session.login("admin", "secret")
+    app.contact.modify_first_contact(Contact("first_name1", "middle_name1", "last_name1", "roga_and_kopyta1", "891112345671",
+                             "something1@mail.ru", "www.wwwww1.com", "SPb1", "work_place1"))
+    app.session.logout()
+
