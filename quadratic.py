2b7d5aecdd125824e0e0954c9aebd4bcafe3d95b
Maria <Maria@yandex.ru>
2022-04-30 21:33:26 +0300

    test_modify_group.py
    modify_first_group updated: used is not construction, method is splitted

---
+++ quadratic.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+
+def solve(a, b, c):
+    d = b*b - 4*a*c
+
+    # if d < 0:
+    #     print("No solutions")
+    # else:
+    #     if d == 0:
+    #         x = -b / (2*a)
+    #         print ("One solutions: " + str(x))
+    #     else:
+    #         x1 = (-b + sqrt(d)) / (2*a)
+    #         x2 = (-b - sqrt(d)) / (2*a)
+    #         print("Two solutions " + str(x1) + " and " + str(x2))
+
+    if d < 0:
+        print("No solutions")
+    elif d == 0:
+        x = -b / (2*a)
+        print ("One solutions: " + str(x))
+    elif d > 0:
+        x1 = (-b + sqrt(d)) / (2*a)
+        x2 = (-b - sqrt(d)) / (2*a)
+        print("Two solutions " + str(x1) + " and " + str(x2))
+    else:
+        print("Wrong")
+
+solve(1, 1, 1)
+solve(1, 2, 1)
+solve(1, 5, 6)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,7 +3,19 @@ from model.group import Group
 
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="modified_name", header="modified_header", footer="modified_footer"))
+    app.group.modify_first_group(Group(name="modified_name111", header="modified_header111", footer="modified_footer111"))
+    app.session.logout()
+
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="6666"))
+    app.session.logout()
+
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="8888"))
     app.session.logout()
 
 
