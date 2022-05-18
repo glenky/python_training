bff187fbd380b927500a53926d2094f0b4f24bc0
Maria <Maria@yandex.ru>
2022-05-19 00:34:26 +0300

    delete/modify random group: tests are updated, modify_group_by_index was created

--- fixrure/group.py
+++ fixrure/group.py
@@ -23,20 +23,22 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         wd.get("http://localhost/addressbook/group.php")
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
-    def modify_first_group(self, new_group_data):
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # press edit group
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
@@ -45,9 +47,16 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def modify_first_group(self, new_group_data):
+        self.modify_group_by_index(0, new_group_data)
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def fill_group_form(self, group):
         wd = self.app.wd
