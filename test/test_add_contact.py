7c2dd473290d55f8f3627efcbdc4db12b527f96b
Maria <Maria@yandex.ru>
2022-05-18 11:03:41 +0300

    lists: add compare for contacts testing, all other tests: amount of contacts before and amount of contacts after change

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,6 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    print(old_contacts)
     app.contact.add_new(Contact("first_name", "middle_name", "last_name", "roga_and_kopyta", "89111234567",
                              "something@mail.ru", "www.wwwww.com", "SPb", "work_place"))
     new_contacts = app.contact.get_contact_list()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -6,14 +6,20 @@ def test_modify_contact(app):
     if app.contact.count() == 0:
         app.contact.add_new(Contact("first_name_first", "middle_name1_first", "last_name1_first", "roga_and_kopyta1_first", "891112345671_first",
                              "first_something1@mail.ru", "www.wwwww1.com_first", "SPb1_first", "work_place1_first"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact("first_name1", "middle_name1", "last_name1", "roga_and_kopyta1", "891112345671",
                              "something1@mail.ru", "www.wwwww1.com", "SPb1", "work_place1"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_contact_firstname(app):
     if app.contact.count() == 0:
         app.contact.add_new(Contact("first_name_first", "middle_name1_first", "last_name1_first", "roga_and_kopyta1_first", "891112345671_first",
                              "first_something1@mail.ru", "www.wwwww1.com_first", "SPb1_first", "work_place1_first"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(first_name="abrakadabra"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
