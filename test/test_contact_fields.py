f2b049bf33e50c0a599ef229f7405f38425f0bd0
Maria <Maria@yandex.ru>
2022-06-01 11:05:37 +0300

    test test_check_all_contact_fields added check phones is copied to test_check_all_contact_fields

--- test/test_contact_fields.py
+++ test/test_contact_fields.py
@@ -8,7 +8,7 @@ def test_check_all_contact_fields(app):
 
     index = randrange(contact_count)
     # print(str(index))
-
+    index = 1
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
 
@@ -16,10 +16,21 @@ def test_check_all_contact_fields(app):
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_emails_from_homepage == merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                                 filter(lambda x: x is not None,
                                                            [contact.email, contact.email2, contact.email3])))
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                                           [contact.homephone, contact.mobilephone, contact.workphone]))))
+
 
