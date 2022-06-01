f9d067479a4ae7e9baef41e804238b00585f7983
Maria <Maria@yandex.ru>
2022-06-01 15:12:23 +0300

    test test_add_grous: параметризация добавлена model.group в repr добавлены другие парамерты группы (что бы удобнее было читать название гр в пробежавшем тесте)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 # 1.13th lesson - code refactoring, FF
-
+import pytest
 from model.group import Group
 
+testdata = [
+    Group(name="second", header="second1", footer="second2"),
+    Group(name="", header="", footer="")
+]
 
-def test_add_group(app):
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     group = Group(name="second", header="second1", footer="second2")
     app.group.create(group)
@@ -14,14 +20,6 @@ def test_add_group(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    assert len(old_groups)+1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
